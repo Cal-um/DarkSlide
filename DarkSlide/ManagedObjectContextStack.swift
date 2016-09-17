f859cddbba8c27a295dfa84ca0464494752a3a32
Calum Harris <harris.calum@gmail.com>
2016-09-17 11:25:45 +0100

    Add managedObjectContextStack and added implementation to AppDelegate

---
+++ DarkSlide/ManagedObjectContextStack.swift
@@ -0,0 +1,32 @@
+//
+//  ManagedObjectContextStack.swift
+//  DarkSlide
+//
+//  Created by Calum Harris on 17/09/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct ManagedObjectContextStack {
+	
+	// This pattern was taken from ObjC's book on CoreData. There are two contexts connected to the same PSC. One acts on the main contexts and the other on a background context. This allows concurrent actions to work on the background. When the background context saves an NSNotification is picked up by the mainContext and the changes are merged.
+	
+	let mainContext: NSManagedObjectContext!
+	let backgroundContext: NSManagedObjectContext!
+	
+	init(mainManagedObjectContext mainContext: NSManagedObjectContext) {
+		self.mainContext = mainContext
+		self.backgroundContext = mainContext.createBackgroundContext()
+	}
+	
+	fileprivate func setUpNotificationsForBackgroundContext() {
+		
+		let notificationCentre = NotificationCenter.default
+		notificationCentre.addObserver(forName: NSNotification.Name.NSManagedObjectContextDidSave, object: backgroundContext, queue: nil) { notification in
+			self.mainContext.mergeChanges(fromContextDidSave: notification)
+		}
+	}
+}
+
